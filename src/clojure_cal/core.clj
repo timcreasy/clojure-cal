f3df801db3549e8c3a22f1e78fe50931f695ef8d
Tim Creasy <timcreasy@me.com>
2016-12-01 13:39:16 -0600

    Month functionality working

--- src/clojure_cal/core.clj
+++ src/clojure_cal/core.clj
@@ -2,5 +2,16 @@
   (:require [print-month.core :as pm])
   (:gen-class))
 
-(defn -main [& args]
-  (pm/print-month 12 2016))
+(defn no-arg []
+  (let [date-string (.format (java.text.SimpleDateFormat. "MM yyyy") (new java.util.Date))]
+    (let [date-coll (clojure.string/split date-string #" ")]
+      (let [cur-month (Integer. (first date-coll)) cur-year (Integer. (last date-coll))]
+        (pm/print-month cur-month cur-year)))))
+
+(defn month-year [month year]
+  (let [month (Integer. month) year (Integer. year)]
+    (pm/print-month month year)))
+
+(defn -main
+  ([] (no-arg))
+  ([month year] (month-year month year)))
